0dc0208c60083af86f29a0dd554ee5af91e80195
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-10-08 17:16:03 -0300

    Added login page to project and first configurations

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCH2yQ8DjvfByLull1093CMZfr9ggzbCaY",
+  authDomain: "controle-financeiro-55d4a.firebaseapp.com",
+  projectId: "controle-financeiro-55d4a",
+  storageBucket: "controle-financeiro-55d4a.appspot.com",
+  messagingSenderId: "172482414362",
+  appId: "1:172482414362:web:dcefda501c5cbd99b24ea3"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export { firebase }
---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login-container">
+    <div class="welcome">
+      <h1>{{ title }}</h1>
+      <h3>{{ slogan }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LoginScreen',
+  data: () => ({
+    title: 'PennyWise',
+    slogan: 'O jeito inteligente de controlar o seu dinheiro'
+  })
+}
+</script>
+
+<style scoped>
+.login-container {
+  min-height: 100vh;
+  background-image: url("../assets/loginBackground.svg");
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.welcome {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
